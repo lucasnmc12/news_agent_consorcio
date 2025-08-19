eb904732466c4ae6fc6debd397e20135cac00316
lucasnmc12 <lucas.nogueira20mc@gmail.com>
2025-08-19 10:56:26 -0300

    refinamento da busca do serper

--- tests/test_serper.py
+++ tests/test_serper.py
@@ -4,5 +4,5 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from utils.search_serper import buscar_noticias_serper, formatar_resultados_serper
 
-noticias = buscar_noticias_serper("Banco Central")
+noticias = buscar_noticias_serper("consórcios")
 print(formatar_resultados_serper(noticias))
